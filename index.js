96ae7f83bdcb189732a8026f4455c48204106966
Gislaine Pacheco <gislainepacheco18@gmail.com>
2024-04-18 21:47:40 -0300

    add routes

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require("express");
 
+const express = require("express");
 const app = express();
 
-app.get("/contato", function (req, res){
-    res.send("página de contato");
+// route
+// get -> receber dados
+// post -> enviar dados
 
+
+app.get("/listar-tarefas", function(req, res){
+    res.send("tarefas cadastradas");
 });
 
-app.listen(8000);
+app.post("/nova-tarefa", function(req, res){
+    res.send("tarefa adicionada");
+});
+
+app.post("/alterar-tarefa", function(req, res) {
+    res.send("tarefa alterada")
+});
+
+app.post("/deletar-tarefa", function(req, res){
+    res.send("tarefa deletada");
+});
+
+app.post("/logar", function(req, res){
+    res.send("usuario logado");
+})
+
+app.listen(8000, function(){
+    console.log("Servidor iniciado")
+});
